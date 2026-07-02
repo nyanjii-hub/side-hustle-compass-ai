cf5b8331a1bd72dd4347493dd3731addcd90fc35
yasu <yh.workad@gmail.com>
2026-07-02 23:19:06 +0900

    feat: add diagnosis page

---
+++ app/diagnosis/page.tsx
@@ -0,0 +1,6 @@
+// app/diagnosis/page.tsx
+import { DiagnosisContainer } from "@/components/diagnosis/DiagnosisContainer";
+
+export default function DiagnosisPage() {
+  return <DiagnosisContainer />;
+}
